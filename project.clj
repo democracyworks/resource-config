ba5b2d265fe71b05f9a001bf19ab6371c7e435b7
Chris Shea <cmshea@gmail.com>
2015-07-02 10:00:01 -0400

    [CS] Prep 0.2.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject turbovote.resource-config "0.2.0"
+(defproject turbovote.resource-config "0.2.1-SNAPSHOT"
   :description "Simple (too simple?) configuration handling"
   :url "http://github.com/turbovote/resource-config"
   :license {:name "Eclipse Public License"
