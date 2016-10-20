62aa588e4c36de0cb36cf05ed71ee95e3bfd978e
Troy Hill <troy@democracy.works>
2016-10-20 15:36:52 -0600

    Add dependencies

--- project.clj
+++ project.clj
@@ -3,5 +3,14 @@
   :url "http://github.com/turbovote/resource-config"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
-  :profiles {:test {:resource-paths ["test-resources"]}})
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [clj-http "2.3.0"]
+                 [org.clojure/core.cache "0.6.5"]]
+  :profiles {:dev {:resource-paths ["test-resources"]
+                   :dependencies [[http-kit "2.2.0"]
+                                  [compojure "1.5.1"]
+                                  [javax.servlet/servlet-api "2.5"]]}
+             :test {:resource-paths ["test-resources"]
+                    :dependencies [[http-kit "2.2.0"]
+                                   [compojure "1.5.1"]
+                                   [javax.servlet/servlet-api "2.5"]]}})
