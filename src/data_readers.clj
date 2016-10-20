74922ee7be4090bf0c960f001b1225fd91837a4d
Troy Hill <troy@democracy.works>
2016-10-20 15:37:05 -0600

    new url data reader

--- src/data_readers.clj
+++ src/data_readers.clj
@@ -1,2 +1,3 @@
 {resource-config/env turbovote.resource-config.data-readers/env
- resource-config/edn turbovote.resource-config.data-readers/edn}
+ resource-config/edn turbovote.resource-config.data-readers/edn
+ resource-config/url turbovote.resource-config.data-readers/url}
