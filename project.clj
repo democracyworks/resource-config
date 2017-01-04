7fe70e28ae837e97e7287c98c87bbc5ccea2205c
Dave Della Costa <ddellacosta@gmail.com>
2017-01-04 17:04:07 -0500

    update project.clj w/release config

--- project.clj
+++ project.clj
@@ -4,4 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
-  :profiles {:test {:resource-paths ["test-resources"]}})
+  :profiles {:test {:resource-paths ["test-resources"]}}
+  :deploy-repositories [["releases" :clojars]
+                        ["snapshots" :clojars]])
