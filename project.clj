bdf1c7254a8c83c45e2627a130d9be77646eb1db
Wes Morgan <wesmorgan@icloud.com>
2018-10-31 12:21:15 -0600

    Version 1.0.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject democracyworks/resource-config "1.0.0"
+(defproject democracyworks/resource-config "1.0.1-SNAPSHOT"
   :description "Simple (too simple?) configuration handling"
   :url "http://github.com/democracyworks/resource-config"
   :license {:name "Eclipse Public License"
