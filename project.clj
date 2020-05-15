bf7a99da0a2f25bf4775af11260b5e680151f0db
cthompson <cthompson@democracy.works>
2020-05-15 14:13:28 +0000

    v1.1.1 bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject democracyworks/resource-config "1.1.0"
+(defproject democracyworks/resource-config "1.1.1"
   :description "Simple (too simple?) configuration handling"
   :url "http://github.com/democracyworks/resource-config"
   :license {:name "Eclipse Public License"
